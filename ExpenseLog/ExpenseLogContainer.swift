06af66906a6a7ea6e09c1adf13b5315ab99123b8
Femi Aliu <femialiu713@gmail.com>
2023-11-15 12:08:27 +0100

    created persistent container, and datamodel file

--- ExpenseLog/ExpenseLogContainer.swift
+++ ExpenseLog/ExpenseLogContainer.swift
@@ -5,4 +5,13 @@
 //  Created by Femi Aliu on 15/11/2023.
 //
 
-import Foundation
+import CoreData
+
+class ExpenseLogContainer {
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "Expenses")
+        persistentContainer.loadPersistentStores { _, _ in }
+    }
+}
