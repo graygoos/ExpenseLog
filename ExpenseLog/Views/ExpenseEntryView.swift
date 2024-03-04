25c217945cee490ec5058999e02ecdadd2e0b874
Femi Aliu <femialiu713@gmail.com>
2024-03-04 12:45:17 +0100

    ExpenseListView now shows list for expenses for any given date Also edited itemUnits array in ExpenseEntryView

--- ExpenseLog/Views/ExpenseEntryView.swift
+++ ExpenseLog/Views/ExpenseEntryView.swift
@@ -27,7 +27,7 @@ struct ExpenseEntryView: View {
     @State private var expenseFrequency = "One-time"
     @State private var expenseCurrency = "NGN"
     
-    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Bag", "Box", "Bar", "Bottles", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrapper", "Foil", "Container", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Tube", "Bucket", "Sachet", "Cup", "Other"]
+    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Bag", "Box", "Bar", "Bottle", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrapper", "Foil", "Container", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Centilitre", "Tube", "Bucket", "Sachet", "Cup", "Other"]
     
     var paymentMethod = ["Debit Card", "Cash", "Electronic Funds Transfer", "Credit Card", "Cheque",  "Cryptocurrency"]
     
--- ExpenseLog/Views/ExpenseListView.swift
+++ ExpenseLog/Views/ExpenseListView.swift
@@ -5,97 +5,30 @@
 //  Created by Femi Aliu on 05/02/2024.
 //
 
-//import SwiftUI
-//
-//struct ExpenseListView: View {
-//    var date: Date
-//
-//    var body: some View {
-//        List(expenses, id: \.self) { expense in
-//            // Display expense details here
-//            Text(expense.itemName ?? "")
-//        }
-//        .navigationTitle("Expenses on \(date.formattedDay)")
-//}
-//
-//#Preview {
-//    ExpenseListView()
-//}
 
-/*
 import SwiftUI
 
 struct ExpenseListView: View {
     let date: Date
-//    @FetchRequest(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: false)], predicate: NSPredicate(format: "expenseDate >= %@ AND expenseDate < %@", argumentArray: [date, Calendar.current.date(byAdding: .day, value: 1, to: date)!]), animation: .default) var expenses: FetchedResults<ExpensesEntity>
     
-    @FetchRequest(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)], predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any]), animation: .default) var expenses: FetchedResults<ExpensesEntity>
-      
-//       @FetchRequest var expenses: FetchedResults<ExpensesEntity>
-       
-       init(date: Date?) {
-           self.date
-           _expenses = FetchRequest(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)],
-                                    predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any]), animation: .default)
-       }
-
-    var body: some View {
-        List(expenses, id: \.self) { expense in
-            // Display expense details here
-            Text(expense.itemName ?? "")
-        }
-        .navigationTitle("Expenses on \(date.formattedDay)")
-    }
-}
-
-*/
-
-
-import SwiftUI
-
-struct ExpenseListView: View {
-    var date: Date?
+    @FetchRequest var expenses: FetchedResults<ExpensesEntity>
     
-//    var expenses: FetchedResults<ExpensesEntity>
-    
-//    @FetchRequest(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)], predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any]), animation: .default) var expenses: FetchedResults<ExpensesEntity>
-    
-   /*
-    @FetchRequest(
-        entity: ExpensesEntity.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)],
-        predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any]),
-        animation: .default
-    ) var expenses: FetchedResults<ExpensesEntity>
-    */
-    
-    init(date: Date?) {
+    init(date: Date) {
         self.date = date
-//        expenses = FetchRequest<ExpensesEntity>(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)],
-//                                 predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any]), animation: .default)
+        let predicate = NSPredicate(format: "expenseDate >= %@ AND expenseDate < %@", argumentArray: [date, Calendar.current.date(byAdding: .day, value: 1, to: date)!])
+        _expenses = FetchRequest(entity: ExpensesEntity.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: false)], predicate: predicate, animation: .default)
     }
-   
+
     var body: some View {
         VStack {
-//            Text("\(expenses.count) expenses")
-//            Text(self.date.formattedDay ?? "No date")
+
             List {
-//                ForEach(expenses, id: \.self) { expense in
-//                    Text("\(expense.itemAmount ?? 0) a text")
-    //                Text("a text")
-//                }
+                ForEach(expenses, id: \.self) { expense in
+                    Text("\(expense.itemAmount ?? 0)")
+                }
             }
-            .navigationTitle("Expenses for \(date?.formattedDay ?? "")")
+            .navigationTitle("Expenses for \(date.formattedDay)")
         }
-//        .onAppear {
-//            let request = FetchRequest<ExpensesEntity>(
-//                entity: ExpensesEntity.entity(),
-//                sortDescriptors: [NSSortDescriptor(keyPath: \ExpensesEntity.expenseDate, ascending: true)],
-//                predicate: NSPredicate(format: "expenseDate == %@", argumentArray: [date as Any])
-////                animation: .default
-//            )
-//            request.execute()
-//        }
     }
 }
 
