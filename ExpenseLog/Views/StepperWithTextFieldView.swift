9f4d65774dd26eb0af451d2e1a6aac470298e595
Femi Aliu <femialiu713@gmail.com>
2024-03-13 12:18:06 +0100

    beginnings of Stepper with TextField

---
+++ ExpenseLog/Views/StepperWithTextFieldView.swift
@@ -0,0 +1,55 @@
+//
+//  StepperWithTextFieldView.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 13/03/2024.
+//
+
+import SwiftUI
+
+struct StepperWithTextFieldView: View {
+    @Binding var itemQty: Int
+    @State private var textValue: String = ""
+    
+    var body: some View {
+        HStack {
+            Button("-") {
+                if itemQty > 1 {
+                    itemQty -= 1
+//                    textValue = "\(itemQty)"
+                    updateTextValue()
+                }
+            }
+            .padding()
+            
+            TextField("Quantity", text: $textValue)
+                .keyboardType(.decimalPad)
+                .onAppear {
+                    textValue = "\(itemQty)"
+                }
+                .onChange(of: textValue) { newValue in
+                    if let quantity = Int(newValue) {
+                        itemQty = quantity
+                    }
+                }
+                .frame(width: 50)
+            
+            Button("+") {
+                itemQty += 1
+//                textValue = "\(textValue)"
+                updateTextValue()
+            }
+            .padding()
+        }
+    }
+    
+    private func updateTextValue() {
+        textValue = "\(itemQty)"
+    }
+}
+
+//#Preview {
+//    @State var quantity: Int = 1
+//    
+//    StepperWithTextFieldView(itemQty: $quantity)
+//}
