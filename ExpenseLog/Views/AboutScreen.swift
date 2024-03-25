1a886579b9382d55a0e91c8119614724f2bdfe2e
Femi Aliu <femialiu713@gmail.com>
2024-03-25 17:14:34 +0100

    created AboutScreen to show abou details of the app in SettingsScreen

---
+++ ExpenseLog/Views/AboutScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AboutScreen.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 25/03/2024.
+//
+
+import SwiftUI
+
+struct AboutScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
