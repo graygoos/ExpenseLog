f6218b48dad81dc0b4fba00e0ccf9ac431ef3e0c
Femi Aliu <femialiu713@gmail.com>
2024-03-25 17:27:47 +0100

    Created ExportExpenseDetailsScreen file

---
+++ ExpenseLog/Views/ExportExpenseDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ExportExpenseDetailsScreen.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 25/03/2024.
+//
+
+import SwiftUI
+
+struct ExportExpenseDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExportExpenseDetailsScreen()
+}
