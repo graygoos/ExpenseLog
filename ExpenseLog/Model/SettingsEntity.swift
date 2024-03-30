17444060824da9893ef6190fdaf71d3592eaa90b
Femi Aliu <femialiu713@gmail.com>
2024-03-30 13:41:21 +0100

    created SettingsEntity class in model folder to use CoreData for user settings

---
+++ ExpenseLog/Model/SettingsEntity.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsEntity.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 30/03/2024.
+//
+
+import Foundation
+import CoreData
+
+class SettingsEntity: NSManagedObject {
+    @NSManaged var showPaymentDetailsSection: Bool
+    @NSManaged var showQuantitySection: Bool
+    @NSManaged var showVendorSection: Bool
+    @NSManaged var showLocationSection: Bool
+    @NSManaged var showDescriptionSection: Bool
+    @NSManaged var showFrequencySection: Bool
+    @NSManaged var showCategorySection: Bool
+}
