40da399813241a62758f6c212885d7bbd87cb370
Femi Aliu <femialiu713@gmail.com>
2024-04-04 13:57:10 +0100

    added about text for Settings tab of app

--- ExpenseLog/Views/AboutScreen.swift
+++ ExpenseLog/Views/AboutScreen.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct AboutScreen: View {
-    let aboutText = "ExpenseLog is an expense logging app"
+    let aboutText = """
+Welcome to ExpenseLog – your personal expense tracking companion! Developed to simplify daily expense recording, ExpenseLog was born out of a personal need to manage expenses more effectively. 
+As one of our esteemed testers, you're among the first to experience ExpenseLog's intuitive features and functionalities as they develop. Over the next 90 days, we're committed to delivering weekly updates, introducing new features, refining existing ones, and addressing your valuable feedback. Our ultimate aim? To launch ExpenseLog on the App Store, complete with versions optimized for iPadOS and macOS.
+Rest assured, your data's safety is our priority. ExpenseLog enables seamless data export, with robust security measures in place to protect your information.
+We value your insights and invite you to actively participate in testing ExpenseLog. Your feedback plays a crucial role in shaping the app's future. Together, let's redefine expense tracking.
+Thank you for joining us on this journey!
+
+Femi
+"""
     
     var body: some View {
-        HStack {
-            Text("ExpenseLog ")
-            Image(systemName: "creditcard.fill")
+        ScrollView {
+            HStack {
+                Text(aboutText)
+            }
+            .padding()
         }
     }
 }
