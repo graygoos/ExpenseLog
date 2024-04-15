3ce5e16d1ee34f3ae8ad6ff3fd15e0bf15ed9898
Femi Aliu <femialiu713@gmail.com>
2024-04-15 13:40:22 +0100

    deleted commented out code

--- ExpenseLog/Views/ExpenseEntryView.swift
+++ ExpenseLog/Views/ExpenseEntryView.swift
@@ -14,26 +14,14 @@ struct ExpenseEntryView: View {
     
     @State private var model = ExpenseParameters()
     
-//    @FetchRequest(entity: SettingsEntity.entity(), sortDescriptors: []) var settings: FetchedResults<SettingsEntity>
-    
     @Binding var settings: Settings
-        
-    
-//    @State private var showPaymentDetailsSection =      false
-//    @State private var showQuantitySection =            false
-//    @State private var showVendorSection =              false
-//    @State private var showLocationSection =            false
-//    @State private var showDescriptionSection =         false
-//    @State private var showFrequencySection =           false
-//    @State private var showCategorySection =            false
-    
+
     var body: some View {
         NavigationStack {
             ExpenseFormView(model: $model, settings: $settings)
                 .navigationTitle("Enter expense")
                 .onAppear {
                     self.model.moc = moc
-//                    updateSettings()
                 }
                 .toolbar {
                     ToolbarItem(placement: .topBarLeading) {
@@ -57,24 +45,6 @@ struct ExpenseEntryView: View {
                 }
         }
     }
-    
-//    private func updateSettings() {
-//        if let settingsEntity = $settings.first {
-//            model.showPaymentDetailsSection = settingsEntity.showPaymentDetailsSection
-//            model.showQuantitySection = settingsEntity.showQuantitySection
-//            model.showVendorSection = settingsEntity.showVendorSection
-//            model.showLocationSection = settingsEntity.showLocationSection
-//            model.showDescriptionSection = settingsEntity.showDescriptionSection
-//            model.showFrequencySection = settingsEntity.showFrequencySection
-//            model.showCategorySection = settingsEntity.showCategorySection
-//        }
-//        do {
-//        try moc.save()
-//        
-//    } catch {
-//        print("Error updating setting: \(error.localizedDescription)")
-//    }
-//    }
 }
 
 #Preview {
