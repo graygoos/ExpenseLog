552fa0544625aee75f20d5768de550d3a7432871
Femi Aliu <femialiu713@gmail.com>
2024-06-17 12:27:25 +0100

    fixed issue where expenses from various expenses are summed up together expenses from different currencies are summed separately

--- ExpenseLog/Views/TodayViews/TodayExpenseSectionFooter.swift
+++ ExpenseLog/Views/TodayViews/TodayExpenseSectionFooter.swift
@@ -11,52 +11,35 @@ struct TodayExpenseSectionFooter: View {
     
     var expenses: FetchedResults<ExpensesEntity>
     
-//    @Binding var total: Decimal
-    
-    var dailyTotal: Decimal {
-        expenses.reduce(0) { result, expense in
-            expense.itemAmount! as Decimal + result
+    private func totalsByCurrency() -> [String: Decimal] {
+        var totals: [String: Decimal] = [:]
+        
+        for expense in expenses {
+            let currency = expense.expenseCurrency ?? "NGN"
+            let amount = expense.itemAmount! as Decimal
+            totals[currency, default: 0] += amount
         }
-//        return 0
+        
+        return totals
     }
     
     var body: some View {
-        HStack {
+        VStack(alignment: .leading) {
             Text("Total")
                 .font(.bold(.title2)())
-            Spacer()
-            Text("\(dailyTotal, format: .currency(code: Locale.current.currency?.identifier ?? "NGN"))")
-                .font(.bold(.title2)())
-            // Text(((expense.itemAmount))! as Decimal, format: .currency(code: expense.expenseCurrency ?? "NGN"))
-        }
-        /*
-        VStack(alignment: .trailing) {
-            ForEach(Array(Set(expenses.compactMap { $0.expenseCurrency })), id: \.self) { currency in
-                if let currency = currency {
-                    Text("\(currency): \(totalForCurrency(currency: currency), format: .currency(code: currency))")
+            
+            ForEach(totalsByCurrency().sorted(by: { $0.key < $1.key }), id: \.key) { currency, total in
+                HStack {
+                    Spacer()
+                    Text("\(total, format: .currency(code: currency))")
                         .font(.bold(.title2)())
                 }
             }
         }
-         */
     }
-    /*
-    // Function to calculate the total for each currency
-    func totalForCurrency(currency: String) -> Decimal {
-        expenses.reduce(Decimal(0)) { result, expense in
-            if expense.expenseCurrency == currency {
-                return (expense.itemAmount! as Decimal) + result
-            } else {
-                return result
-            }
-        }
-    }
-     */
 }
 
 #Preview {
-//    let moc = ExpenseLogContainer(forPreview: true).persistentContainer.viewContext
-    
     @FetchRequest<ExpensesEntity>(
         sortDescriptors: [],
             predicate: NSPredicate(format: "%K >= %@ AND %K <= %@", argumentArray: [#keyPath(ExpensesEntity.expenseDate), Date().startOfDay as NSDate, #keyPath(ExpensesEntity.expenseDate), Date().endDayOf as NSDate]),
