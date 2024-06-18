1671d342164710da7f1e32130e62912c03c31d88
Femi Aliu <femialiu713@gmail.com>
2024-06-18 22:26:33 +0100

    added "Canister" to itemUnits

--- ExpenseLog/Model/ExpenseParameters.swift
+++ ExpenseLog/Model/ExpenseParameters.swift
@@ -56,7 +56,7 @@ class ExpenseParameters {
     var expenseCurrency = "NGN"
     var expenseCategory = "Non"
     
-    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Bag", "Box", "Bar", "Loaf", "Card", "Bottle", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrapper", "Foil", "Container", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Centilitre", "Tube", "Bucket", "Sachet", "Cup", "Other"]
+    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Bag", "Box", "Bar", "Loaf", "Card", "Bottle", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrapper", "Foil", "Container", "Canister", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Centilitre", "Tube", "Bucket", "Sachet", "Cup", "Other"]
     
     var paymentMethod = ["Debit Card", "Cash", "Electronic Funds Transfer", "Credit Card", "Cheque",  "Cryptocurrency", "Bank App", "USSD"]
     
