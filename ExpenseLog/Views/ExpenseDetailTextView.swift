e7963a3d3d598613c1354bfbfc83e503c3207ea3
Femi Aliu <femialiu713@gmail.com>
2024-07-01 12:14:41 +0100

    Created ExpenseDetailTextView

---
+++ ExpenseLog/Views/ExpenseDetailTextView.swift
@@ -0,0 +1,31 @@
+//
+//  ExpenseDetailTextView.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 01/07/2024.
+//
+
+import SwiftUI
+
+struct ExpenseDetailTextView: View {
+    var title: String
+    var detail: String
+    var isCurrency: Bool = false
+    var currencyCode: String? = nil
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .foregroundStyle(.secondary)
+            if isCurrency, let code = currencyCode {
+                Text(Decimal(string: detail) ?? 0, format: .currency(code: code))
+            } else {
+                Text(detail)
+            }
+        }
+    }
+}
+
+#Preview {
+    ExpenseDetailTextView(title: "", detail: "")
+}
