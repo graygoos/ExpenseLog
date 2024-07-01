c3c3592fbd897d39cb1dff8106114ae83f6d801a
Femi Aliu <femialiu713@gmail.com>
2024-07-01 22:35:09 +0100

    added SF symbol to indicate payment type in ExpenseListView

--- ExpenseLog/Views/ExpenseListView.swift
+++ ExpenseLog/Views/ExpenseListView.swift
@@ -34,6 +34,7 @@ struct ExpenseListView: View {
                         ExpenseDetailsView(expense: expense, model: $model, settings: $settings)
                     } label: {
                         HStack {
+                            Image(systemName: symbolForPaymentType(expense.paymentType ?? ""))
                             VStack(alignment: .leading) {
                                 Text("\(expense.viewItemName)")
                                     .font(.title3)
@@ -73,6 +74,29 @@ struct ExpenseListView: View {
         try? moc.save()
         dismiss()
     }
+    
+    private func symbolForPaymentType(_ paymentType: String) -> String {
+            switch paymentType {
+            case "Debit Card":
+                return "creditcard.fill"
+            case "Cash":
+                return "banknote.fill"
+            case "Electronic Funds Transfer":
+                return "arrow.right.arrow.left.circle.fill"
+            case "Credit Card":
+                return "creditcard.fill"
+            case "Cheque":
+                return "checkmark.rectangle.fill"
+            case "Cryptocurrency":
+                return "bitcoinsign.circle.fill"
+            case "Bank App":
+                return "iphone.homebutton"
+            case "USSD":
+                return "number"
+            default:
+                return "creditcard.fill"
+            }
+        }
 }
 
 
