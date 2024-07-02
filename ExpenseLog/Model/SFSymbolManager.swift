519da4bf21c768c805dd87196da4bb88f8aa4fb2
Femi Aliu <femialiu713@gmail.com>
2024-07-02 11:45:23 +0100

    added symbolForCategory function to SFSymbolManager struct

--- ExpenseLog/Model/SFSymbolManager.swift
+++ ExpenseLog/Model/SFSymbolManager.swift
@@ -31,4 +31,57 @@ struct SFSymbolManager {
             return "creditcard.fill"
         }
     }
+    
+    static func symbolForCategory(_ category: String) -> String {
+        switch category {
+        case "Non":
+            return "questionmark.circle"
+        case "Food & Dining":
+            return "fork.knife"
+        case "Groceries":
+            return "cart.fill"
+        case "Transportation":
+            return "car.fill"
+        case "Utilities (Electricity, Water, Gas)":
+            return "bolt.fill"
+        case "Rent":
+            return "house.fill"
+        case "Mortgage":
+            return "house.circle.fill"
+        case "Entertainment":
+            return "music.note"
+        case "Healthcare":
+            return "heart.fill"
+        case "Health & Fitness":
+            return "figure.walk"
+        case "Shopping":
+            return "bag.fill"
+        case "Personal Care":
+            return "person.crop.circle"
+        case "Travel":
+            return "airplane"
+        case "Insurance":
+            return "shield.fill"
+        case "Education":
+            return "book.fill"
+        case "Debt Payments":
+            return "banknote.fill"
+        case "Investments":
+            return "chart.bar.fill"
+        case "Gifts & Donations":
+            return "gift.fill"
+        case "Home Maintenance":
+            return "hammer.fill"
+        case "Pet Care":
+            return "pawprint.fill"
+        case "Taxes":
+            return "doc.text"
+        case "Subscriptions & Memberships":
+            return "doc.text.below.ecg"
+        case "Miscellaneous":
+            return "ellipsis.circle"
+        default:
+            return "questionmark.circle"
+        }
+    }
 }
