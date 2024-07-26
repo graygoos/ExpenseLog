0c00da3870913dbf139d2d6c669bfd3a282bdf47
Femi Aliu <femialiu713@gmail.com>
2024-07-26 16:14:14 +0100

    created DeletedExpensesView

---
+++ ExpenseLog/Views/DeletedExpensesView.swift
@@ -0,0 +1,18 @@
+//
+//  DeletedExpensesView.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 26/07/2024.
+//
+
+import SwiftUI
+
+struct DeletedExpensesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeletedExpensesView()
+}
