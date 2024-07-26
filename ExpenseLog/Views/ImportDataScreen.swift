4914802adc4dcb15109c0480bb03e54265c2471b
Femi Aliu <femialiu713@gmail.com>
2024-07-26 15:56:36 +0100

    created ImportDataScreen

---
+++ ExpenseLog/Views/ImportDataScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ImportDataScreen.swift
+//  ExpenseLog
+//
+//  Created by Femi Aliu on 26/07/2024.
+//
+
+import SwiftUI
+
+struct ImportDataScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImportDataScreen()
+}
