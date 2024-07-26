6ecac60b39ddf0c73cec165c2df65cd48d120d87
Femi Aliu <femialiu713@gmail.com>
2024-07-26 14:23:44 +0100

    added progress view to FeebackFormView

--- ExpenseLog/Views/FeedbackFormView.swift
+++ ExpenseLog/Views/FeedbackFormView.swift
@@ -6,13 +6,22 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct FeedbackFormView: View {
+    @State private var isLoading = true
+    
     var body: some View {
-        WebView(urlString: "https://docs.google.com/forms/d/e/1FAIpQLScdKRWA8d4NC6nc3_2w62jJhQMZzNL-18_WERSHtMLwPMluXw/viewform?usp=sf_link")
-            .edgesIgnoringSafeArea(.all)
-            .toolbar(.hidden, for: .tabBar)
+        ZStack {
+            WebView(urlString: "https://docs.google.com/forms/d/e/1FAIpQLScdKRWA8d4NC6nc3_2w62jJhQMZzNL-18_WERSHtMLwPMluXw/viewform?usp=sf_link", isLoading: $isLoading)
+                .edgesIgnoringSafeArea(.all)
+            
+            if isLoading {
+                ProgressView()
+                    .scaleEffect(1.5)
+                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+            }
+        }
+        .toolbar(.hidden, for: .tabBar)
     }
 }
 
