eff9a7f6038e4b3582aa51f51a0859c0acac33f7
Femi Aliu <femialiu713@gmail.com>
2024-08-01 17:43:10 +0100

    added yard, meter and measure to itemUnits

--- ExpenseLog/Model/ExpenseParameters.swift
+++ ExpenseLog/Model/ExpenseParameters.swift
@@ -63,7 +63,7 @@ class ExpenseParameters {
         self.expenseDate = expenseDate
     }
     
-    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Crate", "Bag", "Box", "Bar", "Loaf", "Card", "Bottle", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrap", "Wrapper", "Foil", "Container", "Canister", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Portion", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Centilitre", "Tube", "Bucket", "Basket", "Sachet", "Cup", "Page", "Stick", "Unit", "Other"]
+    var itemUnits = ["Non", "Pack", "Tin", "Carton", "Crate", "Bag", "Box", "Bar", "Loaf", "Card", "Bottle", "Jar", "Can", "Bowl", "Piece", "Plate", "Case", "Bulk Container", "Pouch", "Blister Pack", "Wrap", "Wrapper", "Foil", "Container", "Canister", "Envelope", "Cellophane/Plastic wrap", "Bushel", "Pair", "Portion", "Kilogram", "Roll", "Pound", "Ounce", "Litre", "Centilitre", "Yard", "Meter", "Tube", "Bucket", "Basket", "Sachet", "Cup", "Page", "Stick", "Measure", "Unit", "Other"]
     
     var paymentMethod = ["Debit Card", "Cash", "Electronic Funds Transfer", "Credit Card", "Cheque",  "Cryptocurrency", "Bank App", "USSD"]
     
